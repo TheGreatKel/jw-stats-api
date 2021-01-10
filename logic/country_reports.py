0088d2e5962bdabd37e994d3113dccad7fe48ca9
Kelven Opoku <kelvenopoku@gmail.com>
2021-01-09 19:15:47 -0500

    added files

---
+++ logic/country_reports.py
@@ -0,0 +1,4 @@
+def read(year):
+    #some logic to take care of a (optional) specific country
+    json = 3 # found out how to return json file
+    return json, 200
---
+++ logic/grand_totals.py
@@ -0,0 +1,4 @@
+def read(year):
+     #some logic to take care of a (optional) year range
+     json = bue #findout how to input json
+    return json, 200
