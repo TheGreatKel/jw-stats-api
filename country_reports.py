e93be84874827935496d7c0fc8f62d2765827090
Kelven Opoku <kelvenopoku@gmail.com>
2021-01-21 09:11:03 -0500

    refactored read methods

--- country_reports.py
+++ country_reports.py
@@ -1,13 +1,11 @@
 import json
 
 def read(year, country=None):
-	if country:# there happens to be a country
-		with open(f"./jsons/country_reports_{year}.json", "r") as reports:
+	with open(f"./jsons/country_reports_{year}.json", "r") as reports:
 			lst_of_dics = json.loads(reports.read())
+	if country:
 		for dic in lst_of_dics:
 			if dic['Country or Territory'] == country:
 				return dic
-	else:
-		with open(f"./jsons/country_reports_{year}.json", "r") as reports:
-			return json.loads(reports.read())
+	return lst_of_dics
     
--- grand_totals.py
+++ grand_totals.py
@@ -1,13 +1,12 @@
 import json
 def read(year, end_year=None):
-#      if end_year:# there happens to be an end year
-#           #then we have to combine jsons. Sounds fun!
-#         with open(f".jsons/country_reports_{year}.json", "r") as reports:
-#             return json.loads(reports.read())[country]
-#     else:
-
     with open("./jsons/grand_totals.json", "r") as reports:
         lst_of_dics = json.loads(reports.read())
+    if end_year:
+      #  for num in range(year,end_year+1): [num for num in range(year,end_year)]
+      pass
+
+    else:
         for dic in lst_of_dics:
             if dic['Year'] == year:
                 return dic
