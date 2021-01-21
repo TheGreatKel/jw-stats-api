5956382918a2f6bd948de2d99971e310f47bbab6
Kelven Opoku <kelvenopoku@gmail.com>
2021-01-21 15:44:44 -0500

    all methods working

--- grand_totals.py
+++ grand_totals.py
@@ -1,21 +1,22 @@
 import json
 def read(year, other_year=None):
-    with open("./jsons/grand_totals.json", "r") as reports:
-        lst_of_dics = json.loads(reports.read())
-    if other_year:
-      look_up = {}
-      res = []
-      if other_year>year:
-		for num in range(year,other_year+1):
-			look_up[num] = None
-      else:
-      	for num in range(other_year,year+1):
-      		look_up[num] = None
-      for dic in lst_of_dics:
-      	if dic["Year"] in look_up:
-      		res.append(dic)
-    else:
-        for dic in lst_of_dics:
-            if dic['Year'] == year:
-                return dic
+	with open("./jsons/grand_totals.json", "r") as reports:
+		lst_of_dics = json.loads(reports.read())
+	if other_year:
+		look_up = {}
+		res = []
+		if other_year>year:
+			for num in range(year,other_year+1):
+				look_up[num] = None
+		else:
+			for num in range(other_year,year+1):
+				look_up[num] = None
+		for dic in lst_of_dics:
+			if dic["Year"] in look_up:
+				res.append(dic)
+		return res	
+	else:
+		for dic in lst_of_dics:
+			if dic['Year'] == year:
+				return dic
         
