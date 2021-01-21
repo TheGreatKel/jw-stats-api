3c7ae055410b24fa3470e8573aca53f193f0ff60
Opoku James <opokujames@Opokus-iMac.home>
2021-01-21 09:41:49 -0500

    added other_year parameter

--- grand_totals.py
+++ grand_totals.py
@@ -1,11 +1,19 @@
 import json
-def read(year, end_year=None):
+def read(year, other_year=None):
     with open("./jsons/grand_totals.json", "r") as reports:
         lst_of_dics = json.loads(reports.read())
-    if end_year:
-      #  for num in range(year,end_year+1): [num for num in range(year,end_year)]
-      pass
-
+    if other_year:
+      look_up = {}
+      res = []
+      if other_year>year:
+		for num in range(year,other_year+1):
+			look_up[num] = None
+      else:
+      	for num in range(other_year,year+1):
+      		look_up[num] = None
+      for dic in lst_of_dics:
+      	if dic["Year"] in look_up:
+      		res.append(dic)
     else:
         for dic in lst_of_dics:
             if dic['Year'] == year:
